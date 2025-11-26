5472ca034d543f56ee15d1a5c8f25623a6bfb90f
rfqhmmwn <rifqihimmawan@gmail.com>
2025-11-26 15:52:12 +0700

    init

---
+++ form_add.php
@@ -0,0 +1,53 @@
+<?php
+    include 'inc/header.php';   
+?>
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+        <div class="card">
+            <form>
+                <div class="card-body">
+                    <div class="form-group">
+                        <label for ="exampleInputUsername">Username</label>
+                        <input type="text" class="form-control" id="exampleInputUsername" aria-describedby="usernameHelp" placeholder="Enter username">
+                    </div>
+                    <div class="form-group">
+                        <label for ="exampleInputPassword">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword" aria-describedby="passwordHelp" placeholder="Enter password">
+                    </div>
+                    <div class="form-group">
+                        <label for ="exampleInputEmail">Email</label>
+                        <input type="email" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter email">
+                    </div>
+                    <div class="form-group">
+                        <label for ="exampleInputFirstName">First Name</label>
+                        <input type="text" class="form-control" id="exampleInputFirstName" aria-describedby="firstnameHelp" placeholder="Enter first name">
+                    </div>
+                    <div class="form-group">
+                        <label for ="exampleInputLastName">Last Name</label>
+                        <input type="text" class="form-control" id="exampleInputLastName" aria-describedby="lastnameHelp" placeholder="Enter last name">
+                    </div>
+                    <div class="form-group">
+                        <label for ="exampleInputCompany">Company</label>
+                        <input type="text" class="form-control" id="exampleInputCompany" aria-describedby="companyHelp" placeholder="Enter company">
+                    </div>
+                    <div class="form-group">
+                        <label for ="exampleInputPhone">Phone</label>
+                        <input type="text" class="form-control" id="exampleInputPhone" aria-describedby="phoneHelp" placeholder="Enter phone">
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <!-- /.container-fluid -->
+
+<?php
+    include 'inc/footer.php';   
+?>
+
+
